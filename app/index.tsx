c89ba970fadeec296f6e23e94d5f6873ab4e39ec
Natig <natigagharzayev@gmail.com>
2025-09-21 12:56:01 +0300

    feat: setup nativewind and layout, create safe frame

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,16 @@
 import { View, Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import React from 'react'
+import "../global.css"
 
 export default function HomeScreen() {
     return (
-        <View>
-            <Text>index</Text>
-        </View>
+        <SafeAreaView className='bg-red-300 h-full'>
+            <View>
+                <Text className='text-4xl text-center'>
+                    Pomodoro app
+                </Text>
+            </View>
+        </SafeAreaView>
     )
 }
