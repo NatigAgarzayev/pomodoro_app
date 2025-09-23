3e311259c2029331244057fa8e01897e4cfd5c09
NatigAgharzayev <natigagharzayev@gmail.com>
2025-09-23 12:24:00 +0300

    feat(status): add pahzes and partial handling

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,19 @@
 import { View, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import React from 'react'
+import React, { useState } from 'react'
 import "../global.css"
 import PhazeStatus from '@/components/pages/main/PhazeStatus'
 
+const scenario = ['work', 'short_break', 'work', 'short_break', 'work', 'short_break', 'work', 'long_break'] as const
+
 export default function HomeScreen() {
+    const [phaze, setPhaze] = useState<'work' | 'short_break' | 'long_break'>('work')
+
+
     return (
         <SafeAreaView className='bg-pink-wall h-full'>
-            <View>
-                <PhazeStatus />
+            <View className='flex-1 justify-center items-center'>
+                <PhazeStatus phaze={phaze} />
             </View>
         </SafeAreaView >
     )
