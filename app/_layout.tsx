4a33e35e8236af28f41204ce82c88ee61438a004
NatigAgharzayev <natigagharzayev@gmail.com>
2025-10-05 14:38:13 +0300

    feat: remove sentry

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,35 +2,16 @@ import React, { useState } from 'react'
 import { Slot } from 'expo-router'
 import Theme from '@/components/provider/Theme'
 import * as SplashScreen from 'expo-splash-screen'
-import * as Sentry from '@sentry/react-native'
 
-Sentry.init({
-    dsn: 'https://554c3b4802f3262ed0bc6fd88f5366ad@o4510098384945152.ingest.de.sentry.io/4510098386321488',
-
-    // Adds more context data to events (IP address, cookies, user, etc.)
-    // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
-    sendDefaultPii: true,
-
-    // Enable Logs
-    enableLogs: true,
-
-    // Configure Session Replay
-    replaysSessionSampleRate: 0.1,
-    replaysOnErrorSampleRate: 1,
-    integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
-
-    // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-    // spotlight: __DEV__,
-})
 SplashScreen.setOptions({
     duration: 3000,
     fade: true,
 })
 
-export default Sentry.wrap(function RootLayout() {
+export default function RootLayout() {
     return (
         <Theme name="default">
             <Slot />
         </Theme>
     )
-})
+}
